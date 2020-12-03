725bf28cdbc58a82f0ca82e8a4c4fb86c464ea84
Michael Jones <mjones@kotrs.com>
2020-12-03 17:15:30 -0500

    Added check for current state before attempting to set state + get state operation

--- src/main/java/net/mikej/connectors/internal/operations/FlowOperations.java
+++ src/main/java/net/mikej/connectors/internal/operations/FlowOperations.java
@@ -4,6 +4,7 @@ import net.mikej.connectors.api.FlowState;
 import org.mule.runtime.api.artifact.Registry;
 import org.mule.runtime.api.exception.MuleException;
 import org.mule.runtime.core.api.construct.Flow;
+import org.mule.runtime.core.api.lifecycle.LifecycleState;
 import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
 import org.mule.runtime.extension.api.annotation.param.reference.FlowReference;
 
@@ -26,13 +27,32 @@ public class FlowOperations {
         SetFlowState(flowName, FlowState.STARTED);
     }
 
+    public FlowState GetFlowState(
+            @DisplayName("Flow Name") @FlowReference String flowName
+    ) throws Exception {
+        Flow flow = (Flow) muleRegistry.lookupByName(flowName).get();
+        LifecycleState currentState = flow.getLifecycleState();
+        if (currentState.isStarted() || currentState.isStarting()) return FlowState.STARTED;
+        else if (currentState.isStopped() || currentState.isStopping()) return FlowState.STOPPED;
+
+        throw new Exception("Unknown flow state encountered");
+    }
+
     public void SetFlowState(
             @DisplayName("Flow Name") @FlowReference String flowName,
             @DisplayName("State")FlowState state
     ) throws MuleException {
         Flow flow = (Flow) muleRegistry.lookupByName(flowName).get();
-        if (state == FlowState.STARTED) flow.start();
-        else flow.stop();
+        LifecycleState currentState = flow.getLifecycleState();
+
+        if (state == FlowState.STARTED) {
+            if (currentState.isStarted() || currentState.isStarting()) return;
+            flow.start();
+        }
+        else {
+            if (currentState.isStopped() || currentState.isStopped()) return;
+            flow.stop();
+        }
     }
 
 }
